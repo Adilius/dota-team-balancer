4964cf77dcfb4fa2b8d7e77f91534d05d84cca0f
AdilAboulkacim <adil.aboulkacim@consid.se>
2023-09-08 12:37:07 +0200

    Team rating advantage sorting

--- algorithm_balancer.py
+++ algorithm_balancer.py
@@ -1,8 +1,12 @@
 from itertools import combinations
 from statistics import mean
+import json
+from collections import OrderedDict
 
+
+# Mockup data - Player name : Player rating
 players = {
-    "Alpha": 6000,
+    "Alpha": 6000, 
     "Bravo": 5220,
     "Charlie": 4820,
     "Delta": 4300,
@@ -14,45 +18,66 @@ players = {
     "Juliett": 2600,
 }
 
-players_numbers = [
-     6000,
-     5220,
-    4820,
-     4300,
-    4100,
-    3500,
-    3300,
-     2900,
-     2700,
-    2600
-]
-
+# Get all subsets of combinations with 5 items from a set of 10 players
 combinations = list(combinations(players,5))
 
-for index, combination in enumerate(combinations):
-    print(f'Matchup number: {index}')
+# Get only combinations which player 1 is present (removes inverse combinations which leads to duplicate matchups)
+unique_combinations = list()
+for combination in combinations:
+    if next(iter(players)) in combination:
+        unique_combinations.append(combination)
+combinations = unique_combinations
 
+# Store unique matchups
+matchups = dict()
+
+# Create matchup data
+for index, combination in enumerate(combinations):
+    
+    # Create unique matchup index
+    matchup_number = index
+
+    # Team 1
     team_1_players = list(combination)
     team_1_ratings = [players[player] for player in team_1_players]
     team_1_mean = mean(team_1_ratings)
-    print(f'Team 1 players: {team_1_players}')
-    print(f'Team 1 ratings: {team_1_ratings}')
-    print(f'Team 1 average: {team_1_mean}')
 
+    # Team 2
     team_2_players = [player for player in players.keys() if not player in combination]
     team_2_ratings = [players[player] for player in team_2_players]
     team_2_mean = mean(team_2_ratings)
-    print(f'Team 2 players: {team_2_players}')
-    print(f'Team 2 ratings: {team_2_ratings}')
-    print(f'Team 2 average: {team_2_mean}')
 
+    # Team advantage
     rating_advantage = abs(team_1_mean-team_2_mean)
     team_advantage = "1" if team_1_mean>team_2_mean else "2"
 
-    print(f'Rating advantage: {rating_advantage} for team {team_advantage}')
+    # Create dict for matchup
+    matchup = {
+        matchup_number : {
 
-    print('\n\n')
+        
+        "Rating advantage" : rating_advantage,
+        "Team advantage" : team_advantage,
+        "Team 1" : {
 
-    #print(f'Average: {sum(combination)/len(combination)}')
-#print(combinations)
+            "Players": team_1_players,
+            "Ratings": team_1_ratings,
+            "Average rating": team_1_mean
+        },
+        "Team 2" : {
+
+            "Players": team_2_players,
+            "Ratings": team_2_ratings,
+            "Average rating": team_2_mean
+        }
+    }
+    }
+    matchups.update(matchup)
+
+# Sort matchups depending on rating advantage
+sorted_matchups = OrderedDict(sorted(matchups.items(), key=lambda x: x[1]['Rating advantage']))
+
+
+# Print matchup information
+print(json.dumps(sorted_matchups, indent=4))
 print(f'Number of combinations: {len(combinations)}')
